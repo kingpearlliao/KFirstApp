b0cf345e5e4b25231bff52a5fae760c13bedf392
KingPearl Liao <you@example.com>
2018-04-19 17:05:10 +0800

    一頁app

---
+++ 一頁精美的app畫面/一頁精美的app畫面/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  一頁精美的app畫面
+//
+//  Created by Kingpearl on 2018/4/19.
+//  Copyright © 2018年 Kingpearl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
